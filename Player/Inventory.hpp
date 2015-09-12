6463ad43706e0c2dd83fb195b0f82c401deaf1a8
Julian <julzso23@gmail.com>
2015-09-12 16:36:11 +0100

    Set integers to unsigned

--- Player/Inventory.hpp
+++ Player/Inventory.hpp
@@ -7,12 +7,12 @@ class Inventory
 {
 private:
     std::vector<ItemBase> items;
-    int maxSize;
+    unsigned int maxSize;
 public:
-    Inventory(int size);
+    Inventory(unsigned int size);
     ~Inventory();
 
     bool addItem(ItemBase& item);
-    bool removeItem(int id);
-    ItemBase& getItem(int id);
+    bool removeItem(unsigned int id);
+    ItemBase& getItem(unsigned int id);
 };
