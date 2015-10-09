17e197846662ffb4761a3b4b912e9c2dc516d3ad
Julian <julzso23@gmail.com>
2015-10-09 13:13:33 +0100

    Draw first inventory item. Player object - I don't even...

--- SurvivalGame.cpp
+++ SurvivalGame.cpp
@@ -13,3 +13,8 @@ void SurvivalGame::initialise(JE::Window& window)
 {
     window.create(sf::VideoMode::getDesktopMode(), "Survival Game", sf::Style::None);
 }
+
+void SurvivalGame::draw(JE::Window& window) const
+{
+    window.draw(*((Player)player).getInventory()->getItem(0));
+}
--- SurvivalGame.hpp
+++ SurvivalGame.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <Engine\AppBase.hpp>
+#include <Player\Player.hpp>
 
 class SurvivalGame : public JE::AppBase
 {
 private:
+    Player player;
+
     void initialise(JE::Window& window);
+    void draw(JE::Window& window) const;
 public:
     SurvivalGame();
     ~SurvivalGame();
